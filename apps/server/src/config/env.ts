7e8a19712b75a925cf3abc75a9ca6f72ad92ab4b
Kay Kim <kimjh@bawi.org>
2022-04-19 11:57:30 +0900

    Fix number defualting

--- apps/server/src/config/env.ts
+++ apps/server/src/config/env.ts
@@ -5,7 +5,7 @@ config();
 
 const envSchema = z.object({
   NODE_ENV: z.enum(['production', 'development', 'test']).default('development'),
-  PORT: z.preprocess((v) => Number(v), z.number().default(8080)),
+  PORT: z.preprocess((v) => Number(v || 8080), z.number()),
   REDIS: z.string().default('localhost:6399'),
 });
 
