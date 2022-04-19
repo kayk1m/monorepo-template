66212e2029d4ce615bbce6e613385c9d6caaf359
Kay Kim <kimjh@bawi.org>
2022-04-19 11:37:52 +0900

    Make 'development' as a default NODE_ENV

--- apps/server/src/config/env.ts
+++ apps/server/src/config/env.ts
@@ -4,7 +4,7 @@ import { z } from 'zod';
 config();
 
 const envSchema = z.object({
-  NODE_ENV: z.enum(['production', 'development', 'test']),
+  NODE_ENV: z.enum(['production', 'development', 'test']).default('development'),
   PORT: z.number().default(8080),
   REDIS: z.string().url().default('localhost:6399'),
 });
