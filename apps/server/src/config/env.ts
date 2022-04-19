04ab62da824108ecb0992c7cc708822004b0b9b9
Kay Kim <kimjh@bawi.org>
2022-04-19 11:52:42 +0900

    Remove url for REDIS validation

--- apps/server/src/config/env.ts
+++ apps/server/src/config/env.ts
@@ -6,7 +6,7 @@ config();
 const envSchema = z.object({
   NODE_ENV: z.enum(['production', 'development', 'test']).default('development'),
   PORT: z.preprocess((v) => Number(v), z.number().default(8080)),
-  REDIS: z.string().url().default('localhost:6399'),
+  REDIS: z.string().default('localhost:6399'),
 });
 
 export const ENV = envSchema.parse(process.env);
